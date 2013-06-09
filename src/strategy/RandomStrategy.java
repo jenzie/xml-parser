8a5bb691dd8572398284006aba9d3c2d3b015088
Jenny Zhen <coffee@csh.rit.edu>
2013-06-09 18:10:29 -0400

    Started working on the strategies.

--- src/strategy/RandomStrategy.java
+++ src/strategy/RandomStrategy.java
@@ -7,5 +7,30 @@ package strategy;
  * project: xml-parser
  */
 
-public class RandomStrategy {
+public class RandomStrategy implements ApproximationStrategy {
+	private int count;
+
+	@Override
+	/**
+	 * @return the number of variables changed using the strategy.
+	 */
+	public int getCount() {
+		return this.count;
+	}
+
+	@Override
+	/**
+	 * Performs the approximation based on the strategy to replace all
+	 * existing variables.
+	 *
+	 * @param file current file to approximate.
+	 * @param find data type to find
+	 * @param replace data type to replace the original/find with
+	 * @return the root node of the modified tree, which is necessary for
+	 * comparing results of multiple strategies.
+	 */
+	public String[] approximate(String[] file, String find, String replace) {
+		String[] result = file;
+		return result;
+	}
 }
