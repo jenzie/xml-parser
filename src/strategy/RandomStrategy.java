19e6985a392fc3e519bca66aa7e88d7fa75102a6
Jenny Zhen <coffee@csh.rit.edu>
2013-06-10 10:15:47 -0400

    Worked on the random strategy.

--- src/strategy/RandomStrategy.java
+++ src/strategy/RandomStrategy.java
@@ -8,9 +8,12 @@
 package strategy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RandomStrategy implements ApproximationStrategy {
 	private int count;
+	private int totalCount;
 
 	@Override
 	/**
@@ -33,7 +36,42 @@ public class RandomStrategy implements ApproximationStrategy {
 	 */
 	public ArrayList<String> approximate(
 			ArrayList<String> file, String find, String replace) {
-		ArrayList<String> result = file;
+
+		ArrayList<String> result = new ArrayList<String>();
+		Map indices = this.countTotal(file, find);
+
 		return result;
+	}
+
+	public Map countTotal(ArrayList<String> file, String find) {
+		Map indices = new HashMap();
+		String[] tempPiece, tempLine;
+		String tempResult;
+
+		// reset count
+		this.totalCount = 0;
+
+		// n = line
+		for(int n = 0; n < file.size(); n++) {
+			tempLine = file.get(n).split("<");
+			tempResult = tempLine[0];
+
+			for(int i = 1; i < tempLine.length; i++) {
+				tempLine[i] = "<" + tempLine[i];
+				tempPiece = tempLine[i].split(">");
+
+				// j = piece
+				for(int j = 0; j < tempPiece.length; j++) {
+					if(tempPiece[j].contains("<"))
+						tempPiece[j] = tempPiece[j] + ">";
+
+					if(tempPiece[j].equals(find)) {
+						this.totalCount++;
+						indices.put(n, j);
+					}
+				}
+			}
+		}
+		return indices;
 	}
 }
