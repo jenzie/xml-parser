1058996af03eb22eb583615965789caab9f0e8f8
Jenny Zhen <coffee@csh.rit.edu>
2013-06-20 09:11:14 -0400

    Renamed script files.

--- results_test/strategy1/input_src/FFT.h
+++
@@ -1,5 +0,0 @@
-
-void FFT_transform(int N, double *data, int print);
-void FFT_inverse(int N, double *data, int print);
-void FFT_bitreverse(int N, double *data);
-double FFT_num_flops(int N);
--- results_test/strategy1/input_src/MonteCarlo.h
+++
@@ -1,2 +0,0 @@
-double MonteCarlo_integrate(int Num_samples, int print);
-double MonteCarlo_num_flops(int Num_samples);
